054db6758048b8e7711f8ffaf27eb94528b2368f
poomsc <poom.genius@gmail.com>
2020-03-10 21:25:17 +0700

    change formmat page continue moth && create page add

--- main.py
+++ main.py
@@ -1,39 +1,46 @@
 from flask import Flask
-from flask import render_template,redirect,url_for
-from datetime import datetime,timedelta
+from flask import render_template, redirect, url_for
+from datetime import datetime, timedelta
 from flask import request
 
 app = Flask(__name__)
 
-items = { }
+items = {}
 
 @app.route('/')
-def index():  
+def index():
+    return render_template('index.html')
+
+@app.route('/create')
+def create():
     time = str(datetime.now())[0:10].split('-')
     time = time[2] + '-' + time[1] + '-' + time[0]
-    return  render_template('test.html',
-                            time=time,
-                            items=items.values())
+    return render_template('create.html',
+                           time=time,
+                           items=items.values())
+
 
 @app.route('/news/<id>/')
 def show_item(id):
     new_item = items[int(id)]
     return render_template('new_item.html',
-                            id=new_item['id'],
-                            title=new_item['title'],
-                            body=new_item['body'])
+                           id=new_item['id'],
+                           title=new_item['title'],
+                           body=new_item['body'])
 
-def new_items(title,body):
+
+def new_items(title, body):
     new_id = len(items.keys()) + 1
     return {
-            'id' : new_id,
-            'title' : title,
-            'body' : body
+        'id': new_id,
+        'title': title,
+        'body': body
     }
 
-@app.route('/news/create/',methods=['POST'])
+
+@app.route('/news/create/', methods=['POST'])
 def create_new_item():
     item = new_items(request.form['title'],
-                    request.form['body'])
+                     request.form['body'])
     items[item['id']] = item
     return redirect(url_for('index'))
